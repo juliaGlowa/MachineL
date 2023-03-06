5510749ea62e79744ebb0a47d00ccd8da25c9afe
Julia G <juliethejester@gmail.com>
2023-03-06 12:02:36 +0100

    Summary

---
+++ Reprocessing.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+import math
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import train_test_split
+
+df_train = pd.read_csv("", sep = "," , encoding= 'utf-8')
